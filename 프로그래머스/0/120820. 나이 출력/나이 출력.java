ef83c066a36ba68835b95de6133b374dc5d995d0
mindev188 <mindev188@gmail.com>
2025-06-15 17:25:28 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * 2022년 기준 출생 연도 구하기
+     * @param age
+     * @return
+     */
+    public static int solution(int age) {
+        int answer = 2022 - age + 1;
+        return answer;
+    }
+}
