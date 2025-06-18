db89a206a401d22f3ff2a6faba90d7d742a2cc15
mindev188 <mindev188@gmail.com>
2025-06-19 08:21:07 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.04 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        for (char a : my_string.toCharArray()) { 
+            for (int i = 0; i < n; i++) {
+                sb.append(a);
+            }
+        }
+        return sb.toString();
+    }
+}
