f2fe0b221b1b6afcfcc839ae042f5238405c6d43
mindev188 <mindev188@gmail.com>
2025-08-08 07:17:47 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+
+        char tmp;
+        char[] arr = my_string.toCharArray();
+
+        tmp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = tmp;
+        return String.valueOf(arr);
+    }
+}
