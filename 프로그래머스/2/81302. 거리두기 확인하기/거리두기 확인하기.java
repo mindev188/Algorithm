06cf39de4e2f88462d7aa9a4537e12469cfa5d49
mindev188 <mindev188@gmail.com>
2026-07-24 10:24:39 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.68 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class Solution {
+    int[] directionX = {1, -1, 0, 0};
+    int[] directionY = {0, 0, 1, -1};
+
+    public int[] solution(String[][] places) {
+        int[] answer = new int[places.length];
+        Arrays.fill(answer, 1);
+
+        for (int roomNum = 0; roomNum < places.length; roomNum++) {
+            String[] room = places[roomNum];
+            int ySize = room.length;
+            int xSize = room[0].length();
+
+            boolean[][] visited = new boolean[ySize][xSize];
+            Queue<String> queue = new LinkedList<>();
+            for (int i = 0; i < ySize; i++) {
+                for (int j = 0; j < xSize; j++) {
+                    String row = room[i];
+                    String place = String.valueOf(row.charAt(j));
+                    if (visited[i][j] || !"P".equals(place)) continue;
+
+                    visited[i][j] = true;
+                    if (DFS(visited, room, j, i, 0)) {
+                        answer[roomNum] = 0;
+                    }
+
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean DFS(boolean[][] visited, String[] room, int x, int y, int dept) {
+        if (dept >= 2) return false;
+
+        for (int i = 0; i < directionX.length; i++) {
+            int nextY = y + directionY[i];
+            int nextX = x + directionX[i];
+
+            if (nextX >= 0 && nextX < 5 && nextY >= 0 && nextY < 5) {
+                if (visited[nextY][nextX] || room[nextY].charAt(nextX) == 'X') continue;
+                if (room[nextY].charAt(nextX) == 'P') return true;
+                visited[nextY][nextX] = true;
+                if (DFS(visited, room, nextX, nextY, dept + 1)) return true;
+            }
+
+        }
+        return false;
+    }
+}
