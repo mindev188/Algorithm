987a407507cc1a09ff9ed94c7a37046b22fcb432
mindev188 <mindev188@gmail.com>
2026-08-24 15:15:45 +0900

    [level 1] Title: 모의고사, Time: 2.68 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,28 +1,24 @@
-import java.util.ArrayList;
+import java.util.*;
 class Solution {
+    int[] a = {1, 2, 3, 4, 5};
+    int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+    int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
     public int[] solution(int[] answers) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-
-        int[] aAnswer = {1,2,3,4,5};
-        int[] bAnswer = {2,1,2,3,2,4,2,5};
-        int[] cAnswer = {3,3,1,1,2,2,4,4,5,5};
-        int[] scores = new int[3];
-
+        int aCount = 0;
+        int bCount = 0;
+        int cCount = 0;
         for (int i = 0; i < answers.length; i++) {
-            int num = answers[i];
-
-            if (aAnswer[i % aAnswer.length] == num) scores[0]++;
-            if (bAnswer[i % bAnswer.length] == num) scores[1]++;
-            if (cAnswer[i % cAnswer.length] == num) scores[2]++;
+            if (answers[i] == a[i % a.length]) aCount++;
+            if (answers[i] == b[i % b.length]) bCount++;
+            if (answers[i] == c[i % c.length]) cCount++;
         }
 
-        int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
-
-        for (int i = 0; i < 3; i++) {
-            if (max == scores[i]) list.add(i + 1);
-        }
-
+        int maxCount = Math.max(aCount, Math.max(bCount, cCount));
+        List<Integer> list = new ArrayList<>();
+        if (maxCount == aCount) list.add(1);
+        if (maxCount == bCount) list.add(2);
+        if (maxCount == cCount) list.add(3);
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
-
 }
